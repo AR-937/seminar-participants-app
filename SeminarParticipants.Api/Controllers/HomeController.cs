8cd21ad543d9b2318001ba176ee0e1d392d0e3b8
Abdirahman <abdirahmanxabokar@gmail.com>
2024-09-25 13:55:20 +0200

    conf env var AND set up multi docker-compose files

---
+++ SeminarParticipants.Api/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SeminarParticipants.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        // GET: api/<HomeController>
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return _configuration["EnvironmentValue"];
+        }
+    }
+}
--- SeminarParticipants.Api/Program.cs
+++ SeminarParticipants.Api/Program.cs
@@ -21,21 +21,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-//using (var scope = app.Services.CreateScope())
-//{
-//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//    db.Database.Migrate();
-//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-
-    using var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
