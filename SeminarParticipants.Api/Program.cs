75da5d40ea7ed9dbf8e7855a21a2194f82371258
Abdirahman <abdirahmanxabokar@gmail.com>
2024-09-26 11:11:52 +0200

    minor changes to program.cs and .gitignore

--- SeminarParticipants.Api/Program.cs
+++ SeminarParticipants.Api/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SeminarParticipantsDatabaseConnection"));
+    options.UseSqlServer(builder.Configuration
+        .GetConnectionString("SeminarParticipantsDatabaseConnection"));
 });
 
 builder.Host.UseSerilog((context, configuration) =>
